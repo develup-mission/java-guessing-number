9231fd196d9be745a3b4e109dafdfe2ebac1f513
SongMinhyuk <dnfldpden32@gmail.com>
2024-10-13 16:58:34 +0900

    FEAT:생성자, start함수 구현

---
+++ src/main/java/guessingnumber/GuessNumGame.java
@@ -0,0 +1,14 @@
+package guessingnumber;
+
+import java.util.Random;
+
+public class GuessNumGame {
+
+    public GuessNumGame() {
+    }
+
+    public void start() {
+        int Answer=new Random().nextInt(100)+1;
+        int life=3;
+    }
+}
