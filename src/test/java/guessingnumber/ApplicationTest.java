f5a5a227ee4587252fd4fe4f99c53c8d3e7fb544
cherryiJuice <chldlwn711@gmail.com>
2024-10-13 22:34:43 +0900

    test: 게임 결과 테스트 추가

--- src/test/java/guessingnumber/ApplicationTest.java
+++ src/test/java/guessingnumber/ApplicationTest.java
@@ -1,7 +1,59 @@
 package guessingnumber;
 
-import static org.junit.jupiter.api.Assertions.*;
+import guessingnumber.controller.MainController;
+import guessingnumber.util.NumberGenerator;
+import guessingnumber.view.InputView;
+import guessingnumber.view.OutputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 class ApplicationTest {
+    private static final String YES_OPTION = "y";
+    private static final String NO_OPTION = "n";
 
+    @DisplayName("3번 내에 정답 후 종료")
+    @Test
+    void startGame() {
+        try (MockedStatic<NumberGenerator> mockNumberGenerator = Mockito.mockStatic(NumberGenerator.class);
+             MockedStatic<InputView> mockInputView = Mockito.mockStatic(InputView.class);
+             MockedStatic<OutputView> mockOutputView = Mockito.mockStatic(OutputView.class)) {
+
+            mockNumberGenerator.when(NumberGenerator::generateByRandom).thenReturn(42);
+
+            mockInputView.when(InputView::inputNumber).thenReturn(50, 40, 42);
+            mockInputView.when(InputView::inputGameLoop).thenReturn(NO_OPTION);
+
+            MainController controller = new MainController();
+            controller.run();
+
+            mockOutputView.verify(OutputView::printStartMessage);
+            mockOutputView.verify(OutputView::printLowMessage);
+            mockOutputView.verify(OutputView::printHighMessage);
+            mockOutputView.verify(OutputView::printCorrectMessage);
+        }
+    }
+
+    @DisplayName("게임시작 후 재시작")
+    @Test
+    void restartGame() {
+        try (MockedStatic<NumberGenerator> mockNumberGenerator = Mockito.mockStatic(NumberGenerator.class);
+             MockedStatic<InputView> mockInputView = Mockito.mockStatic(InputView.class);
+             MockedStatic<OutputView> mockOutputView = Mockito.mockStatic(OutputView.class)) {
+
+            mockNumberGenerator.when(NumberGenerator::generateByRandom).thenReturn(42, 55);
+
+            mockInputView.when(InputView::inputNumber).thenReturn(42, 55);
+            mockInputView.when(InputView::inputGameLoop).thenReturn(YES_OPTION, NO_OPTION);
+
+            MainController controller = new MainController();
+            controller.run();
+
+            mockOutputView.verify(OutputView::printStartMessage, times(2));
+            mockOutputView.verify(OutputView::printCorrectMessage, times(2));
+        }
+    }
 }
